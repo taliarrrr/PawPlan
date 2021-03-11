c12e6fd0925c177d1c5930961ef525a7e88c9620
Macy <s024275@students.lmsd.org>
2021-03-11 10:59:02 -0500

    Initial Commit

---
+++ PawPlan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PawPlan
+//
+//  Created by Macy Donahoe (student LM) on 3/11/21.
+//  Copyright © 2021 Macy Donahoe (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
