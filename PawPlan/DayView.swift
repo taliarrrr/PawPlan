294cf1039a8b30feb52fc1bb05e929319d41ef95
Talia Lehrer <s017077@students.lmsd.org>
2021-03-25 10:08:26 -0400

    with background

--- PawPlan/DayView.swift
+++ PawPlan/DayView.swift
@@ -14,28 +14,37 @@ struct DayView: View {
     @Binding var days : [Day]
     
     var body: some View {
-        HStack{
-            NavigationLink(destination: DayDetail(day: $day, days: $days)){
-                VStack(alignment: .leading){
-                    Text(day.month).font(.system(size: 30))
-                        .foregroundColor(.orange)
-                        .minimumScaleFactor(0.0001)
-                        .lineLimit(1)
-                    Text(day.day1).font(.system(size: 30))
-                        .minimumScaleFactor(0.0001)
-                        .lineLimit(1)
-                    Text(day.year).font(.system(size: 30))
-                        .foregroundColor(.orange)
-                        .minimumScaleFactor(0.0001)
-                        .lineLimit(1)
-                }
+        ZStack{
+            
+            Image("background").frame(width: 390, height: 100, alignment: .bottomTrailing).cornerRadius(20)
+            
+            HStack{
+                NavigationLink(destination: DayDetail(day: $day, days: $days)){
+                    VStack{
+                        HStack{
+                            Text("   " + day.month).font(.system(size: 30))
+                                .foregroundColor(.orange)
+                                .minimumScaleFactor(0.0001)
+                                .lineLimit(1)
+                            
+                            Text(day.day1).font(.system(size: 30))
+                                .minimumScaleFactor(0.0001)
+                                .lineLimit(1)
+                            
+                            Text(day.year + "   ").font(.system(size: 30))
+                                .foregroundColor(.orange)
+                                .minimumScaleFactor(0.0001)
+                                .lineLimit(1)
+                        }
+                        Text(day.dayOfWeek).font(.system(size: 20))
+                    }
+                    
+                }.background(Color.white.opacity(0.9)).cornerRadius(20).cornerRadius(20)
+                    .padding(.horizontal, 30).frame(width: 300, height: 40, alignment: .center)
             }
-        }.background(Color.white.opacity(0.7))
-            .cornerRadius(20)
-            .padding(.horizontal,10)
+        }
     }
 }
-
 
 struct Card_Previews: PreviewProvider {
     static var previews: some View {
